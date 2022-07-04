f35519d721f6f210ece09bd52894bdb6f571db5b
vascofornas <paypal.modesto.esp@gmail.com>
2022-07-04 21:18:29 +0200

    2022_MOV-MAP_NS_ADMIN_2022007042107

--- proyecto_flutter/lib/pantallas/muro.dart
+++ proyecto_flutter/lib/pantallas/muro.dart
@@ -12,6 +12,8 @@ import 'package:movmap_ns_admin/pantallas/verificar_email.dart';
 
 import '../styles/colors.dart';
 
+
+
 class Muro extends StatefulWidget {
   const Muro({Key? key}) : super(key: key);
 
@@ -26,6 +28,15 @@ class _MuroState extends State<Muro> {
   final auth = FirebaseAuth.instance;
   late User user;
   late Timer timer;
+  late bool esAdmin = false;
+
+  final Stream<DocumentSnapshot> _usersStream = FirebaseFirestore.instance
+      .collection("usuarios")
+      .doc(FirebaseAuth.instance.currentUser!.email)
+      .snapshots();
+
+
+
 
 
 
@@ -39,7 +50,12 @@ class _MuroState extends State<Muro> {
 
 
 
+
+
+
+
   }
+
 
   Future<void> checkEmailVerified() async{
     user = auth.currentUser!;
@@ -60,61 +76,108 @@ class _MuroState extends State<Muro> {
 
 
 
+  Future<void> salir() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
 
+
+    await auth.signOut().then((value) {
+      //regresamos a login
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(), //#0006
+          ));
+
+    });
+  }
 
 
   @override
+
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('usuarios').doc(user.email).snapshots(),
-        builder:  (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return Scaffold(body: Text('Something went wrong'));
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
-                body: Text("Loading"));
-          }
-          {
-            //recibimps tpdps los datos del usuario
-
-            var email = snapshot.data.data()['usuarioEmail'];
-            var alias= snapshot.data.data()['usuarioDisplayName'];
-
-            return Scaffold(
-              backgroundColor: Colors.white,
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                      child: Text("bienvenido ${alias}")
-
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  InkWell(
-                      onTap: () async {
-                        //hacemos logout
-                        FirebaseAuth auth = FirebaseAuth.instance;
-                        await auth.signOut().then((value) {
-                          //regresamos a login
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => LoginScreen(), //#0006
-                              ));
-
-                        });
-
-
-                      },
-                      child: Text("LOGOUT"))
-                ],
-              ),
-            );
-          }
-        } );
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("Administración de Mov-Map ",style: TextStyle(color: Colors.white),),
+        ),
+        body: StreamBuilder<DocumentSnapshot>(
+            stream: _usersStream,
+            builder: (context,  AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Text("Loading");
+              } else if (snapshot.hasError) {
+                return Text('Something went wrong');
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              dynamic data = snapshot.data;
+              bool esAdmin = data['usuarioEsAdministrador'];
+              return  Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    esAdmin ? Column(
+                      children: [
+                        Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/app_icon.png")
+                              )
+                          ),
+                        ),
+                        SizedBox(height: 40,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                                width: 120,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Usuarios",textAlign: TextAlign.center, style: TextStyle(color: Colors.white,fontSize: 18),),
+                                )
+                            ),
+                            Container(
+                                width: 120,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text("Usuarios",textAlign: TextAlign.center, style: TextStyle(color: Colors.white,fontSize: 18),),
+                                )
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                        :
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("no soy administrador"),
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
+
